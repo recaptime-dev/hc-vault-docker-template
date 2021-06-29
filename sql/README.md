2fae7ed761756fc9b3a0d3417a19d6e509d8aa0b
Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>
2021-06-29 22:35:21 +0800

    Update sutff in the sql directory
    Signed-off-by: Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>

---
+++ sql/README.md
@@ -0,0 +1,9 @@
+# Documentation for Initializing PostgresDB for Vault
+
+## With Postgres CLI client
+
+TODO
+
+## With Node.js
+
+TODO
---
+++ sql/knexfile.js
@@ -0,0 +1,44 @@
+// Configuration work in progress!
+const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: DB_NAME,
+      user:     DB_USER,
+      password: DB_PASS
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
