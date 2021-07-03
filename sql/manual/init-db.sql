f7fc40099c4aee1e25599479d3f0a8ea5032bafe
Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>
2021-07-03 21:11:04 +0800

    sql-manual: Fix syntax errors
    Signed-off-by: Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>

--- sql/manual/init-db.sql
+++ sql/manual/init-db.sql
@@ -3,7 +3,7 @@
 
   To initalize with Postgres, please see README.md file. Make sure your Postgres version is 9.5 or above before proceeding.
 */
-CREATE TABLE [IF NOT EXISTS] vault_kv_store (
+CREATE TABLE IF NOT EXISTS vault_kv_store (
   parent_path TEXT COLLATE "C" NOT NULL,
   path        TEXT COLLATE "C",
   key         TEXT COLLATE "C",
@@ -11,9 +11,9 @@ CREATE TABLE [IF NOT EXISTS] vault_kv_store (
   CONSTRAINT pkey PRIMARY KEY (path, key)
 );
 
-CREATE INDEX [IF NOT EXISTS] parent_path_idx ON vault_kv_store (parent_path);
+CREATE INDEX IF NOT EXISTS parent_path_idx ON vault_kv_store (parent_path);
 
-CREATE TABLE [IF NOT EXISTS] vault_ha_locks (
+CREATE TABLE IF NOT EXISTS vault_ha_locks (
   ha_key                                      TEXT COLLATE "C" NOT NULL,
   ha_identity                                 TEXT COLLATE "C" NOT NULL,
   ha_value                                    TEXT COLLATE "C",
